4f93b5072f48942209853a071a8cee15c509e4f2
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-11-26 11:59:49 +0545

    task3.1 completed

---
+++ task3/Printer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    internal class Printer
+    {
+        public void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Print(int number)
+        {
+            Console.WriteLine(number);
+        }
+
+        public void Print(string message, int number)
+        {
+            Console.WriteLine($"Message: {message} and Number: {number}");
+        }
+    }
+}
---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from task3: ");
+
+            Printer printer = new Printer();
+            printer.Print("Hello, World!");
+            printer.Print(36);
+            printer.Print("Hello k chha hau", 36);
+
+            Console.WriteLine("=========================================\n");
+
+
+
+
+        }
+    }
+}
