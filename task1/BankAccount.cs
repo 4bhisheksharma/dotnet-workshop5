bc750a4b4f3ad1539cf23da803f309381ec82937
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-11-26 11:35:30 +0545

    task1 completed

---
+++ task1/BankAccount.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class BankAccount
+    {
+        private int accountNumber;
+        private double balance;
+
+        public int AccountNumber
+        {
+            get { return accountNumber; }
+
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+            private set
+            {
+                if (value < 0)
+                {
+                    balance = value;
+                }
+                else
+                {
+                   Console.WriteLine("Initial balance cannot be negative.");
+                }
+            }
+        }
+
+        public BankAccount(int accountNumber, double initialBalance)
+        {
+            this.accountNumber = accountNumber;
+            Balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Deposit amount must be positive.");
+            }
+            else
+            {
+                balance += amount;
+                Console.WriteLine("Deposit successful. New balance: " + balance);
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Withdrawal amount must be positive.");
+            }
+            else if (amount > balance)
+            {
+                Console.WriteLine("Insufficient funds for this withdrawal.");
+            }
+            else{
+                balance -= amount;
+                Console.WriteLine("Withdrawal successful. New balance: " + balance);
+            }
+        }
+    }
+
+}
--- task1/Program.cs
+++ task1/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello from task1\n");
+
+            BankAccount account = new BankAccount(123456, 2500000.0);
+
+            Console.WriteLine($"Hello User, Account Number: {account.AccountNumber}");
+            Console.WriteLine($"Initial Balance: {account.Balance}\n");
+
+            account.Deposit(50000.0);
+            Console.WriteLine($"Balance after deposit: {account.Balance}");
+
+            account.Withdraw(4000.0);
+            Console.WriteLine($"Balance after withdrawal: {account.Balance}");
         }
     }
 }
