3744293126cc71634130a033ffcb01d373021850
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-11-26 12:22:31 +0545

    task4 completed

---
+++ task4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from task4: ");
+
+            Car car = new Car();
+            Console.WriteLine("For Car: ");
+            car.StartEngine();
+            car.Display();
+            car.StopEngine();
+
+            Bike bike = new Bike();
+            Console.WriteLine("For Bike: ");
+            bike.StartEngine();
+            bike.Display();
+            bike.StopEngine();
+
+
+
+
+        }
+    }
+}
